23354784c6ce1ffd74e5f729054c9fea641e1b39
Priyanshu Kumar <priyanshuknp444@gmail.com>
2026-02-05 00:27:19 +0530

    chore: add missing server env

---
+++ env/server.ts
@@ -0,0 +1,13 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const serverEnv = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'production']),
+    DATABASE_URL: z.string().min(1),
+    NEXTAUTH_SECRET: z.string().min(1),
+    TWITTER_CLIENT_ID: z.string().min(1),
+    TWITTER_CLIENT_SECRET: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env,
+});
